182d0882dacad6f4a49c91bf9aaa06250eacba2f
Asnassta <79850172+Asnassta@users.noreply.github.com>
2021-04-04 06:39:51 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,149 @@
+$(document).ready(function() {
+
+	/*Contacts*/
+		let $contacts = $('.contacts')
+			$contactsOpen = $('.menu__burger')
+			$contactsClose = $('.contacts__close')
+
+
+		$contactsOpen.on('click', function(event) {
+			event.preventDefault();
+			$contacts.slideToggle(400)
+		});
+		$contactsClose.on('click', function() {
+			$contacts.slideToggle(400)
+		});
+
+		/*Content*/
+		let $contentItem = $('.content__item')
+			$contentMore = $('.content__more')
+
+
+		$contentItem.hover(function() {
+			if($(window).width() < 992)
+			{
+			 	$(this).find($contentMore).slideToggle()
+			}
+			else
+			{
+				$(this).toggleClass('big')
+				$contentItem.not($(this)).toggleClass('small');
+			}
+		});
+
+		/*Slider*/
+		$('.slider-products').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		dots: true,
+		prevArrow: $('.arrow-slider__prev--products'),
+		nextArrow: $('.arrow-slider__next--products'),
+		responsive: [
+	{
+		breakpoint: 1281,
+			settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+			dots: true,
+			prevArrow: $('.arrow-slider__prev--products-m'),
+			nextArrow: $('.arrow-slider__next--products-m')
+		}
+	}
+  ]
+		});
+
+		$('.slider-expert').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		dots: true,
+		prevArrow: $('.arrow-slider__prev--expert'),
+		nextArrow: $('.arrow-slider__next--expert'),
+		responsive: [
+	{
+		breakpoint: 1280,
+			settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+			dots: true,
+			prevArrow: $('.arrow-slider__prev--expert-m'),
+			nextArrow: $('.arrow-slider__next--expert-m')
+		}
+	}
+  ]
+		});
+
+		$('.slider-program').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		dots: true,
+		prevArrow: $('.arrow-slider__prev--program'),
+		nextArrow: $('.arrow-slider__next--program'),
+		responsive: [
+	{
+		breakpoint: 1281,
+			settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+			dots: true,
+			prevArrow: $('.arrow-slider__prev--program-m'),
+			nextArrow: $('.arrow-slider__next--program-m')
+		}
+	}
+  ]
+		});
+
+		/*Popup*/
+		let $popupProducts = $('#popup-products')
+			$popupExpert = $('#popup-expert')
+			$popupProgram = $('#popup-program')
+			$showExpert = $('#show-expert')
+			$showProgram = $('#show-program')
+			$showProducts = $('#show-products')
+			$closePopup = $('.popup__close');
+
+			$showProducts.on('click', function(event) {
+				event.preventDefault();
+				$popupProducts.addClass('show')
+			});
+			$closePopup.on('click', function() {
+				$popupProducts.removeClass('show')
+			});
+
+			$showExpert.on('click', function(event) {
+				event.preventDefault();
+				$popupExpert.addClass('show')
+			});
+			$closePopup.on('click', function() {
+				$popupExpert.removeClass('show')
+			});
+
+			$showProgram.on('click', function(event) {
+				event.preventDefault();
+				$popupProgram.addClass('show')
+			});
+			$closePopup.on('click', function() {
+				$popupProgram.removeClass('show')
+			});
+
+
+
+		
+
+});
+
+
+$(window).load(function() {
+	$(".after_load").each(function () {
+	    var $back = $(this).attr("data-src");
+	
+	        $(this).prop('style','background-image: url('+$back+');');
+	
+	});
+});
+	
